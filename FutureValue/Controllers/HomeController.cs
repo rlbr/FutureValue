82a2458094f9119c3846f94e46e9032fa96b8be1
Raphael Roberts <raphael.roberts48@gmail.com>
2023-04-17 21:47:02 -0500

    Went through all slides. No styling yet

--- FutureValue/Controllers/HomeController.cs
+++ FutureValue/Controllers/HomeController.cs
@@ -1,14 +1,21 @@
+using FutureValue.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FutureValue.Controllers;
 
 public class HomeController : Controller
-{
+{   [HttpGet]
     // GET
     public IActionResult Index()
     {
-        ViewBag.Name = "Mary";
-        ViewBag.FV = 999999.00;
+        ViewBag.FV = 0;
         return View();
+    }
+
+    [HttpPost]
+    public IActionResult Index(FutureValueModel model)
+    {
+        ViewBag.FV = ModelState.IsValid ? model.CalculateFutureValue() : 0;
+        return View(model);
     }
 }
